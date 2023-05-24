30e3b159d7e13f8397e8b2293a8d80adfeb33f97
Anthony <aretelew@gmail.com>
2023-05-24 18:39:40 -0500

    Removed AI. Working on saving high scores.

--- flappyGame.py
+++ flappyGame.py
@@ -1,7 +1,9 @@
+import os
+
 import pygame
 from sys import exit
 import random
-import neat
+
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -13,7 +15,9 @@ FLOOR_HEIGHT = 520
 GRAVITY = 0.5
 SCROLL_SPEED = 2
 
-window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+window = None
+
 
 # Images
 bird_images = [pygame.image.load("assets/bird_down.png"),
@@ -26,11 +30,11 @@ bottom_pipe_image = pygame.image.load("assets/pipe_bottom.png")
 game_over_image = pygame.image.load("assets/game_over.png")
 start_image = pygame.image.load("assets/start.png")
 
-# Game
+pygame.display.set_icon(bird_images[2])
 
 bird_start_position = (100, 250)
 score = 0
-font = pygame.font.SysFont('Segoe', 26)
+font = pygame.font.SysFont('bahnschrift', 25)
 game_stopped = True
 
 
@@ -44,6 +48,7 @@ class Bird(pygame.sprite.Sprite):
         self.vel = 0
         self.flap = False
         self.alive = True
+        self.dead = False
 
     def update(self, user_input):
         # Animate Bird
@@ -131,6 +136,8 @@ def quit_game():
     # Exit Game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            with open('highscores.txt', 'a') as f:
+                f.write('\n')
             pygame.quit()
             exit()
 
@@ -138,6 +145,7 @@ def quit_game():
 # Game Main Method
 def run_single_player_window():
     global score
+    written = False
 
     # Instantiate Bird
     bird = pygame.sprite.GroupSingle()
@@ -151,6 +159,27 @@ def run_single_player_window():
     x_pos_ground, y_pos_ground = 0, FLOOR_HEIGHT
     ground = pygame.sprite.Group()
     ground.add(Ground(x_pos_ground, y_pos_ground, SCROLL_SPEED))
+
+    high_score = 0
+    with open('highscores.txt', 'r') as file:
+        max = 0
+        max_name = ''
+
+        # Iterate over each line in the file
+        for line in file:
+            print(line.strip().split('|'))
+            try:
+                name, score = line.strip().split('|')
+            except:
+                continue
+            score = int(score)
+
+            # Check if the current score is higher than the previous highest score
+            if score > max:
+                max = score
+                high_score = score
+                max_name = name
+    print(max_name, max)
 
     run = True
     while run:
@@ -190,16 +219,21 @@ def run_single_player_window():
         collision_pipes = pygame.sprite.spritecollide(bird.sprites()[0], pipes, False)
         collision_ground = pygame.sprite.spritecollide(bird.sprites()[0], ground, False)
 
-        print("COLLISION_PIPES", collision_pipes)
-        print("COLLISION_GROUND", collision_ground)
-
         if collision_pipes or collision_ground:
             bird.sprite.alive = False
+            if not written:
+                written = True
+                with open('highscores.txt', 'a') as f:
+                    f.write("|" + str(score))
             if collision_ground:
                 window.blit(game_over_image, (SCREEN_WIDTH // 2 - game_over_image.get_width() // 2,
-                                              SCREEN_HEIGHT // 2 - game_over_image.get_height() // 2))
+                                              SCREEN_HEIGHT // 3 - game_over_image.get_height() // 3))
+
                 if user_input[pygame.K_r]:
                     score = 0
+                    with open('highscores.txt', 'rb+') as f:
+                        f.seek(-2, os.SEEK_END)
+                        f.truncate()
                     break
 
         # Spawn Pipes
@@ -218,141 +252,14 @@ def run_single_player_window():
             pipes.remove(pipes.sprites()[0])
             pipes.remove(pipes.sprites()[0])
 
-        type(pipes.sprites()[0])
-        # print("PASSED")
-
-        pygame.draw.circle(window, (255, 0, 0), (pipes.sprites()[0].rect.x, pipes.sprites()[0].rect.y), 3)
-
-        # print(pipes.sprites().__len__())
-        # print(len(pipes))
-        # print(type(len(pipes.sprites())))
-        # if pipes.sprites().__len__() > 2:
-        # print("TEST")
-        # moving_x = pipes.sprites()[2].rect.x
-        # else:
-        # moving_x = pipes.sprites()[1].rect.x
-
-        # print(pipes.sprites()[1].is_passed())
-
-        # if moving_x < 0:
-        #    moving_x = pipes.sprites()[1].rect.x
-
-        # print(pipes.sprites())
-        # pygame.draw.circle(window, (255, 0, 0), (moving_x, y_bottom), 3)
-
-        # pygame.draw.circle(window, (255, 0, 0), (0,0), 100)
-        # pipes.sprites()[0].
-        # print("PIPE", pipes.sprites()[0].rect.x, pipes.sprites()[0].rect.y)
-
-        clock.tick(60)
-        pygame.display.update()
-
-
-def run_ai_window():
-    def are_alive(bird_list):
-        for bird in birds:
-            if bird.alive:
-                return True
-        return False
-
-    global score
-
-    # Instantiate Bird
-    birds = pygame.sprite.Group()
-    birds.add(Bird())
-
-    # Setup Pipes
-    pipe_timer = 0
-    pipes = pygame.sprite.Group()
-
-    # Instantiate Initial Ground
-    x_pos_ground, y_pos_ground = 0, FLOOR_HEIGHT
-    ground = pygame.sprite.Group()
-    ground.add(Ground(x_pos_ground, y_pos_ground, SCROLL_SPEED))
-
-    run = True
-    while run:
-
-        # Quit
-        quit_game()
-
-        # Reset Frame
-        window.fill((0, 0, 0))
-
-        # User Input
-        user_input = pygame.key.get_pressed()
-
-        # Draw Background
-        window.blit(background, (0, 0))
-
-        # Spawn Ground
-        if len(ground) <= 2:
-            ground.add(Ground(SCREEN_WIDTH, y_pos_ground, SCROLL_SPEED))
-
-        # Draw - Pipes, Ground and Bird
-        pipes.draw(window)
-        ground.draw(window)
-        birds.draw(window)
-
-        # Show Score
-        score_text = font.render('Score: ' + str(score), True, pygame.Color(255, 255, 255))
-        window.blit(score_text, (20, 20))
-
-        # Update - Pipes, Ground and Bird
-        if are_alive(birds):
-            pipes.update()
-            ground.update()
-        birds.update(user_input)
-
-        # Collision Detection
-        for bird in birds:
-            collision_pipes = pygame.sprite.spritecollide(bird, pipes, False)
-            collision_ground = pygame.sprite.spritecollide(bird, ground, False)
-
-        # collision_pipes = pygame.sprite.spritecollide(birds.sprites()[0], pipes, False)
-        # collision_ground = pygame.sprite.spritecollide(birds.sprites()[0], ground, False)
-
-        # print("COLLISION_PIPES", collision_pipes)
-        # print("COLLISION_GROUND", collision_ground)
-
-
-        if collision_pipes or collision_ground:
-            birds.sprites()[0].alive = False
-            if collision_ground:
-                # print("COLLISION GROUND")
-                window.blit(game_over_image, (SCREEN_WIDTH // 2 - game_over_image.get_width() // 2,
-                                              SCREEN_HEIGHT // 2 - game_over_image.get_height() // 2))
-                if user_input[pygame.K_r]:
-                    score = 0
-                    break
-
-        # Spawn Pipes
-        if pipe_timer <= 0 and are_alive(birds):
-            x_top, x_bottom = 550, 550
-            y_top = random.randint(-600, -480)
-            y_bottom = y_top + random.randint(90, 130) + bottom_pipe_image.get_height()
-
-            pipes.add(Pipe(x_top, y_top, top_pipe_image, 'top', SCROLL_SPEED))
-            pipes.add(Pipe(x_bottom, y_bottom, bottom_pipe_image, 'bottom', SCROLL_SPEED))
-            pipe_timer = random.randint(100, 150)
-        pipe_timer -= 1
-
-        pipe_width = pipes.sprites()[0].rect.width
-        if pipes.sprites()[0].rect.x + pipe_width < 0 and pipes.sprites()[1].rect.x + pipe_width < 0:
-            pipes.remove(pipes.sprites()[0])
-            pipes.remove(pipes.sprites()[0])
-
-        # type(pipes.sprites()[0])
-        # print("PASSED")
-
-        # pygame.draw.circle(window, (255, 0, 0), (pipes.sprites()[0].rect.x, pipes.sprites()[0].rect.y), 3)
-
         clock.tick(60)
         pygame.display.update()
 
 
 def menu():
-    global game_stopped
+    global game_stopped, window
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Flappy Bird')
 
     while game_stopped:
         quit_game()
@@ -368,10 +275,19 @@ def menu():
         # User Input
         user_input = pygame.key.get_pressed()
         if user_input[pygame.K_SPACE]:
-            # run_single_player_window()
-            run_ai_window()
+            run_single_player_window()
 
         pygame.display.update()
+    pygame.quit()
 
-# Menu
-menu()
+def start_game():
+    username = input("Enter your name: ")
+    print("The game opened in new window.")
+    with open("highscores.txt", "a") as f:
+        f.write(username)
+    menu()
+
+
+
+start_game()
+#menu()
